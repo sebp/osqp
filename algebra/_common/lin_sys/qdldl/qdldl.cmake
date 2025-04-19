a2374c50b9dd24ba3cff5c7c86a8978d1a166ae7
Sebastian Pölsterl <sebp@k-d-w.org>
2025-04-19 18:22:22 +0200

    Link against qdldl shared library

--- algebra/_common/lin_sys/qdldl/qdldl.cmake
+++ algebra/_common/lin_sys/qdldl/qdldl.cmake
@@ -3,26 +3,7 @@ include(FetchContent)
 message(STATUS "Fetching/configuring QDLDL solver")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
-FetchContent_Declare(
-  qdldl
-  GIT_REPOSITORY https://github.com/osqp/qdldl.git
-  GIT_TAG v0.1.8
-  )
-
-# Make QDLDL use the same types as OSQP
-set(QDLDL_FLOAT ${OSQP_USE_FLOAT} CACHE BOOL "QDLDL Float type")
-set(QDLDL_LONG ${OSQP_USE_LONG} CACHE BOOL "QDLDL Integer type")
-
-# We only want the object library, so turn off the other library products
-set(QDLDL_BUILD_STATIC_LIB OFF CACHE BOOL "Build QDLDL static library")
-set(QDLDL_BUILD_SHARED_LIB OFF CACHE BOOL "Build QDLDL shared library")
-
-FetchContent_MakeAvailable(qdldl)
-FetchContent_GetProperties(qdldl)
-
 list(POP_BACK CMAKE_MESSAGE_INDENT)
-
-set_source_files_properties($<TARGET_OBJECTS:qdldlobject> PROPERTIES GENERATED 1)
 
 file(
     GLOB
@@ -53,6 +34,4 @@ set( LIN_SYS_QDLDL_INC_PATHS
      ${OSQP_ALGEBRA_ROOT}/_common/
      ${OSQP_ALGEBRA_ROOT}/_common/lin_sys/qdldl/
      ${OSQP_ALGEBRA_ROOT}/_common/lin_sys/qdldl/amd/include
-     ${qdldl_SOURCE_DIR}/include
-     ${qdldl_BINARY_DIR}/include
      )
